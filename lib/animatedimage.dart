c14d1cd192f6be793ee3389fdfd73856d8e01057
Shanmukha <55737606+pshanmukha@users.noreply.github.com>
2022-11-24 23:43:24 +0530

    initial commit

---
+++ lib/animatedimage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnimatedImage extends StatefulWidget {
+  const AnimatedImage({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedImageState createState() => _AnimatedImageState();
+}
+
+class _AnimatedImageState extends State<AnimatedImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final notificationBarHeight = MediaQuery.of(context).padding.top;
+    return Container(
+      decoration:const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(145, 131, 222, 1),
+            Color.fromRGBO(160, 148, 227, 1),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              SizedBox(height: notificationBarHeight,),
+              SizedBox(height: 10,),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
